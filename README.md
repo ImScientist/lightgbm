1dfbdaf82f27f8d0e2c70b6f262cb3c1d10b4d6b
Anton Ivanov <a.i.ivanov.sv@gmail.com>
2024-11-18 22:54:20 +0100

    Local optimization (#1)

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # lightgbm
-LightGBM example
+
+Hyperparameter optimization with Optuna.
+
+We use a single fold of the [MSLR-WEB30k](https://www.microsoft.com/en-us/research/project/mslr/) dataset,
+and store it in `$OUTPUT_DIR/data/raw`. Expected folder structure:
+
+```shell
+$OUTPUT_DIR
+├── db.sqlite3
+└── data
+    ├── raw             # raw data (MSLR-WEB30k)
+    │   ├── train.txt
+    │   ├── vali.txt
+    │   └── test.txt
+    └── preprocessed    # preprocessed data
+        ├── train.parquet
+        ├── vali.parquet
+        └── test.parquet
+```
+
+```shell
+export output
+
+PYTHONPATH=src python src/main.py --help
+
+PYTHONPATH=src python src/main.py preprocess-data
+
+PYTHONPATH=src python src/main.py hyperparameter-optimization
+
+optuna-dashboard sqlite:///db.sqlite3
+```
---
+++ src/main.py
@@ -0,0 +1,49 @@
+import os
+import logging
+
+import click
+
+from preprocessing import preprocess_data
+from optimization import hyperparameter_optimization
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)-6s [%(name)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+opj = os.path.join
+
+OUTPUT_DIR = os.environ.get('OUTPUT_DIR', 'output')
+
+DATA_DIR_RAW = opj(OUTPUT_DIR, 'data', 'raw')
+DATA_DIR_PREPROCESSED = opj(OUTPUT_DIR, 'data', 'preprocessed')
+
+
+@click.group()
+def cli():
+    """ cli """
+
+
+@cli.command('preprocess-data')
+@click.option('--data-dir-raw', type=str, default=DATA_DIR_RAW)
+@click.option('--data-dir-preprocessed', type=str, default=DATA_DIR_PREPROCESSED)
+def preprocess_data_fn(data_dir_raw, data_dir_preprocessed):
+    """ Store training data from BQ in GCS """
+
+    preprocess_data(
+        src_dir=data_dir_raw,
+        dst_dir=data_dir_preprocessed)
+
+
+@cli.command('hyperparameter-optimization')
+def hyperparameter_optimization_fn():
+    """ Hyperparameter optimization """
+
+    hyperparameter_optimization(
+        data_dir=DATA_DIR_PREPROCESSED,
+        db_dir=OUTPUT_DIR)
+
+
+if __name__ == "__main__":
+    cli()
